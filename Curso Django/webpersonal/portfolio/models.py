78ba7e5e215e14c8436959dc21c3a598bee12684
Pablo <pablofeijo181@gmail.com>
2019-08-01 00:38:26 -0300

    Cuarto commit, Finalizada Web Personal

--- Curso Django/webpersonal/portfolio/models.py
+++ Curso Django/webpersonal/portfolio/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name = "Titulo")
     description = models.TextField(verbose_name = "Descripcion")
+    more = models.URLField(verbose_name="Mas informacion",null=True, blank=True)
     image = models.ImageField(verbose_name = "Imagen", upload_to="projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name = "Fehca de edicion")
