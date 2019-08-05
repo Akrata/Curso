665ee51b858d1f002e6ab9e53002743c595b0ff8
Pablo <pablofeijo181@gmail.com>
2019-08-04 22:01:03 -0300

    Noveno commit, finalizada app services

--- Curso Django 2(empresarial)/webempresa/webempresa/urls.py
+++ Curso Django 2(empresarial)/webempresa/webempresa/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     #path del admin
     path('admin/', admin.site.urls),
     #path del core
     path('', include('core.urls')),
+    #path del services
+    path('services/', include('services.urls')),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
